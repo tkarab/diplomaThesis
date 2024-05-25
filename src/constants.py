8bca3b35d02feb33e4ce122f15ddcdc18c351299
tkarab <96498608+tkarab@users.noreply.github.com>
2024-05-25 14:46:35 +0300

    feat: add signal separator

---
+++ src/constants.py
@@ -0,0 +1,3 @@
+dataPath = r'C:\Users\ΤΑΣΟΣ\Desktop\Σχολή\Διπλωματική\Δεδομένα'
+ninaproPath = r'C:\Users\ΤΑΣΟΣ\Desktop\Σχολή\Διπλωματική\Δεδομένα\Ninapro'
+separatedDataPath = dataPath + r'\Separated'
