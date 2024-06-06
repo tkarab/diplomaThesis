444cba68ab526befa39e0d912233074c3046c962
tkarab <96498608+tkarab@users.noreply.github.com>
2024-06-06 21:38:23 +0300

    feat: paths added to constants #14

--- src/constants.py
+++ src/constants.py
@@ -3,3 +3,6 @@ import os
 DATA_PATH = r'C:\Users\ΤΑΣΟΣ\Desktop\Σχολή\Διπλωματική\Δεδομένα'
 NINAPRO_PATH = r'C:\Users\ΤΑΣΟΣ\Desktop\Σχολή\Διπλωματική\Δεδομένα\Ninapro'
 SEPARATED_DATA_PATH = os.path.join(DATA_PATH ,'Separated')
+PROCESSED_DATA_PATH_DB2 = r'C:\Users\ΤΑΣΟΣ\Desktop\Σχολή\Διπλωματική\Δεδομένα\processed\db2'
+PROCESSED_DATA_PATH_DB1 = r'C:\Users\ΤΑΣΟΣ\Desktop\Σχολή\Διπλωματική\Δεδομένα\processed\db1'
+PROCESSED_DATA_PATH_DB5 = r'C:\Users\ΤΑΣΟΣ\Desktop\Σχολή\Διπλωματική\Δεδομένα\processed\db5'
