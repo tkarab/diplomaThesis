7987f9eaf87809ece84edf7ba692e792a4d2bbd7
tkarab <96498608+tkarab@users.noreply.github.com>
2024-07-10 23:34:55 +0300

    refactor: removal of layer definition in train.py #15
    they are instead defined in 'model_assembly.py'

--- src/train.py
+++ src/train.py
@@ -42,28 +42,19 @@ class IterationLoggingCallback(keras.callbacks.Callback):
     #         print()
     def on_epoch_end(self, epoch, logs=None):
         super().on_epoch_end(epoch, logs)
-        print('win_size: ', win_size)
+        # print('win_size: ', win_size)
 
-iterations_per_epoch = 1000
+iterations_per_epoch = 10000
 epochs = 25
 win_size = 15
 channels = 12
 inp_shape = (win_size,channels,1)
-cnn_backbone = custom_models.AtzoriNetDB2_embedding_only(input_shape=inp_shape)
+cnn_backbone = custom_models.AtzoriNetDB2_embedding_only(input_shape=inp_shape, add_dropout=True, add_regularizer=True)
                             #simplest_conv_net_1_layer(input_shape = inp_shape, feature_vector_size=12)
 
-model_timeDist = layers.TimeDistributed(cnn_backbone)
 
 #input shape tuple
 inp_shape_5d = (None,) + inp_shape
-
-#Layers
-support_set_inp_shape_layer = layers.Input(inp_shape_5d)
-query_set_inp_shape_layer = layers.Input(inp_shape)
-support_set_embeddings_layer = model_timeDist(support_set_inp_shape_layer)
-query_set_embedding_layer = cnn_backbone(query_set_inp_shape_layer)
-prototypes_layer = Lambda(function=fsl_functions.produce_prototype)(support_set_embeddings_layer)
-query_prediction_layer = Lambda(function=fsl_functions.softmax_classification)([prototypes_layer, query_set_embedding_layer])
 
 ex = '1'
 N = 5
@@ -77,11 +68,9 @@ model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(0
 model2 = model_assembly.assemble_model_reshape(cnn_backbone, inp_shape, way=N, shot=k)
 model2.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(0.001), metrics=['categorical_accuracy'])
 
-
-
 train_loader = TaskGenerator(experiment=ex, way=N, shot=k, mode='train', batches=iterations_per_epoch, print_labels=True, print_lebels_frequency=5)
 
-# [x,y], label = train_loader[0]
+[x,y], label = train_loader[0]
 
 print("END")
 callback = IterationLoggingCallback()
