977e8ec06127baf41cd9b4d5b0da552a7b7fb37b
tkarab <96498608+tkarab@users.noreply.github.com>
2024-08-25 18:49:14 +0300

    feat: TrainingInfoCallback minor additions #57
    - few more parameters read from and added to json file

--- src/custom_callbacks.py
+++ src/custom_callbacks.py
@@ -13,11 +13,12 @@ class IterationLoggingCallback(keras.callbacks.Callback):
         # print('win_size: ', win_size)
 
 class TrainingInfoCallback(keras.callbacks.Callback):
-    def __init__(self, file_path, model,  batch_size, model_filename, model_backbone_name, experiment, iterations_per_epoch, preprocessing_dict, aug_dict, best_epoch_kept = 0):
+    def __init__(self, file_path, model,  batch_size, model_filename, model_backbone_name, experiment, iterations_per_epoch, validation_steps, preprocessing_dict, aug_enabled, aug_dict, data_intake, rms, db,best_epoch_kept = 0):
         super(TrainingInfoCallback, self).__init__()
         self.file_path = file_path
         self.model = model
         self.batch_size = batch_size
+        self.best_epoch_lr = float(self.model.optimizer.learning_rate.numpy())
 
         # 'model_protoNet_1.h5' -> 'model_protoNet_1'
         model_filename = model_filename.split('.')[0]
@@ -29,20 +30,28 @@ class TrainingInfoCallback(keras.callbacks.Callback):
         self.model_backbone_name = model_backbone_name
         self.experiment = experiment
         self.iterations_per_epoch = iterations_per_epoch
+        self.validation_steps = validation_steps
         self.preprocessing_dict = preprocessing_dict
+        self.aug_enabled = aug_enabled
         self.aug_dict = aug_dict
         self.best_epoch_kept = best_epoch_kept
+
+        self.data_intake = data_intake
+        self.rms        = rms
+        self.db         = db
 
     def round_results(self,logs):
         return {key: round(value, 4) for key, value in logs.items()}
 
-    def update_json_log_file(self):
+    def update_json_log_file(self,epoch):
         if os.path.exists(self.json_fullpath):
             with open(os.path.join(self.file_path,self.json_filename), 'r') as f:
                 training_info = json.load(f)
             # training_info["RESULTS"][f"epoch {epoch+1}"] = self.round_results(logs)
-            training_info["TRAINING_INFO"]["TOTAL_EPOCHS"] += 1
+            training_info["TRAINING_INFO"]["TOTAL_EPOCHS"] = epoch+1
             training_info["TRAINING_INFO"]["BEST_EPOCH_KEPT"] = self.best_epoch_kept
+            training_info["TRAINING_INFO"]["LATEST_EPOCH_LEARNING_RATE"] = float(self.model.optimizer.learning_rate.numpy())
+            training_info["TRAINING_INFO"]["BEST_EPOCH_LEARNING_RATE"] = self.best_epoch_lr
 
         else:
             # Create a dictionary with training info
@@ -53,16 +62,26 @@ class TrainingInfoCallback(keras.callbacks.Callback):
                 },
                 "PROCESSING" : {
                     "PREPROCESSING" : self.preprocessing_dict,
+                    "AUG_ENABLED"   : self.aug_enabled,
                     "AUGMENTATION"  : self.aug_dict
                 },
                 "TRAINING_INFO" : {
                     "EXPERIMENT" : self.experiment,
                     "BATCH_SIZE" : self.batch_size,
                     "ITERATIONS_PER_EPOCH" : self.iterations_per_epoch,
+                    "VALIDATION_STEPS" : self.validation_steps,
                     "OPTIMIZER" : self.model.optimizer._name,
-                    "LEARNING_RATE" : float(self.model.optimizer.learning_rate.numpy()),
+                    "LOSS" : self.model.loss,
+                    "METRICS" : self.model.metrics_names,
                     "TOTAL_EPOCHS" : 1,
-                    "BEST_EPOCH_KEPT" : self.best_epoch_kept
+                    "LATEST_EPOCH_LEARNING_RATE": float(self.model.optimizer.learning_rate.numpy()),
+                    "BEST_EPOCH_KEPT" : self.best_epoch_kept,
+                    "BEST_EPOCH_LEARNING_RATE": float(self.model.optimizer.learning_rate.numpy())
+                },
+                "DATA_GENERATOR" : {
+                    "DATA_INTAKE" : self.data_intake,
+                    "RMS" : self.rms,
+                    "DB" : self.db
                 }
             }
         # Save the dictionary as a JSON file
@@ -92,7 +111,7 @@ class TrainingInfoCallback(keras.callbacks.Callback):
 
     # logs has the following form: {'train_loss':1.2, 'train_accuracy':0.6, 'val_loss':1.4, 'val_accuracy':0.5}
     def on_epoch_end(self, epoch, logs=None):
-        self.update_json_log_file()
+        self.update_json_log_file(epoch)
         self.update_txt_results_file(epoch,logs)
 
         return
